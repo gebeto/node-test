f2f7518227f6f26dd2317e4b587ea710a4f7f0a1
gebeto <slavik.nychkalo@gmail.com>
2022-01-12 10:45:39 +0200

    init project with tests

---
+++ main.spec.js
@@ -0,0 +1,15 @@
+const { sum } = require("./main");
+
+describe('sum', () => {
+	it('2 + 2 = 4', () => {
+		expect(sum(2, 2)).toBe(4);
+	});
+
+	it('1 + 2 = 3', () => {
+		expect(sum(1, 2)).toBe(3);
+	});
+
+	it('2 + 2 != 5', () => {
+		expect(sum(2, 2)).not.toBe(5);
+	});
+});
